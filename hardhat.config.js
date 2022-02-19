0efb4324a0b986b1ba69092324eacc9457483643
cedoor <me@cedoor.dev>
2022-02-19 15:38:53 +0100

    docs: add comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,11 @@
 require("@nomiclabs/hardhat-waffle")
 require("hardhat-dependency-compiler")
-require("./tasks/deploy")
+require("./tasks/deploy") // Your deploy task.
 
 module.exports = {
     solidity: "0.8.4",
     dependencyCompiler: {
+        // It allows Hardhat to compile the external Verifier.sol contract.
         paths: ["@appliedzkp/semaphore-contracts/base/Verifier.sol"]
     }
 }
